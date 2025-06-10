ba598bab7c18bd3a6487a85f5c251d4267d42d57
Ankangh001 <ankanghosh010@gmail.com>
2025-06-10 12:11:38 +0530

    update

--- resources/views/admin/blog/edit.blade.php
+++ resources/views/admin/blog/edit.blade.php
@@ -38,6 +38,24 @@
               @csrf
               <div class="card-body">
                 <div class="row">
+                    
+                  @if($blog->blog_image)
+                  <div class="col-md-6">
+                    <div class="form-group text-center">
+                      <b class="d-block mb-3">Preview Blog Image</b>
+                      <img src="{{ asset($blog->blog_image) }}" width="50%">
+                    </div>
+                  </div>
+                  @endif
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label>Blog Image</label>
+                      <input type="file" name="blog_image" class="form-control">
+                    </div>
+                  </div>
+
+                  <hr class="w-100 border-bottom mb-5">
+
                   <div class="col-md-7">
                     <div class="form-group">
                       <label>Title <span style="color:red">*</span></label>
@@ -96,22 +114,6 @@
 
                   <div class="col-md-12">
                     <div class="form-group">
-                      <label>Blog Image</label>
-                      <input type="file" name="blog_image" class="form-control">
-                    </div>
-                  </div>
-
-                  @if($blog->blog_image)
-                  <div class="col-md-12">
-                    <div class="form-group">
-                      <label>Preview Blog Image</label>
-                      <img src="{{ asset($blog->blog_image) }}" width="100px">
-                    </div>
-                  </div>
-                  @endif
-
-                  <div class="col-md-12">
-                    <div class="form-group">
                       <button type="submit" class="btn btn-success float-right">Update</button>
                     </div>
                   </div>
@@ -125,3 +127,31 @@
     </div>
   </section>
 @endsection
+@push('scripts')
+<script src="https://cdn.tiny.cloud/1/p96sr398x0evyp69lvm9o2ieiuyexn462e38v64kghyfl7zy/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+
+<script>
+        // TinyMCE Init - fully featured toolbar (removed hr and toc)
+    tinymce.init({
+      selector: 'textarea.text-editor',
+      height: 500,
+      plugins: 'image media link code table lists advlist fullscreen preview anchor insertdatetime searchreplace wordcount charmap emoticons codesample visualblocks visualchars',
+      toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media codesample | table charmap emoticons | code visualblocks fullscreen preview',
+      images_upload_url: '{{ url("/admin/tinymce/upload") }}',
+      automatic_uploads: true,
+      images_upload_credentials: true,
+      convert_urls: true,
+      relative_urls: false,
+      remove_script_host: false,
+      file_picker_types: 'image',
+      headers: {
+        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]')?.getAttribute('content') || ''
+      },
+      setup: function (editor) {
+        editor.on('change', function () {
+          editor.save();
+        });
+      }
+    });
+</script>
+@endpush
