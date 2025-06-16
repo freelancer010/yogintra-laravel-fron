3dad22b42b07edb5cc41fd235950848e58cd47d7
Ankangh001 <ankanghosh010@gmail.com>
2025-06-16 16:58:04 +0530

    update

--- resources/views/admin/blog/view.blade.php
+++ resources/views/admin/blog/view.blade.php
@@ -35,7 +35,6 @@
                         <th>Image</th>
                         <th>Blog Title</th>
                         <th>Category</th>
-                        <th>View</th>
                         <th>Action</th>
                      </tr>
                   </thead>
@@ -53,21 +52,15 @@
                               @endif
                            </td>
                            <td>{{ $post->category->category_name ?? 'N/A' }}</td>
-                           <td>{{ $post->blog_view }}</td>
                            <td>
                               <div class="input-group-prepend">
-                                 <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                                    Action
-                                 </button>
-                                 <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="{{ route('admin.blog.edit', $post->blog_id) }}">
-                                       <i class="fa fa-edit" aria-hidden="true"></i> Edit
-                                    </a>
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" onclick="return confirm('Are you sure?')" href="{{ route('admin.blog.delete', $post->blog_id) }}">
-                                       <i class="fa fa-trash" aria-hidden="true"></i> Delete
-                                    </a>
-                                 </div>
+                                 <a class="dropdown-item" href="{{ route('admin.blog.edit', $post->blog_id) }}">
+                                    <i class="fa fa-edit" aria-hidden="true"></i> Edit
+                                 </a>
+                                 <div class="dropdown-divider"></div>
+                                 <a class="dropdown-item" onclick="return confirm('Are you sure?')" href="{{ route('admin.blog.delete', $post->blog_id) }}">
+                                    <i class="fa fa-trash" aria-hidden="true"></i> Delete
+                                 </a>
                               </div>
                            </td>
                         </tr>
