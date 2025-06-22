48d225a7a6b878460349ae2f41bd9290cea2c04f
Ankangh001 <ankanghosh010@gmail.com>
2025-06-22 20:27:35 +0530

    update

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -350,46 +350,6 @@
         </div>
     </section>
 
-    <section class="divider bg-gradient_effect d-none">
-        <div class="elementor-shape">
-            <svg xmlns="https://www.w3.org/2000/svg" viewBox="0 0 283.5 27.8" preserveAspectRatio="none">
-                <path class="elementor-shape-fill" d="M283.5,9.7c0,0-7.3,4.3-14,4.6c-6.8,0.3-12.6,0-20.9-1.5c-11.3-2-33.1-10.1-44.7-5.7 
-                s-12.1,4.6-18,7.4c-6.6,3.2-20,9.6-36.6,9.3C131.6,23.5,99.5,7.2,86.3,8c-1.4,0.1-6.6,0.8-10.5,2c-3.8,1.2-9.4,3.8-17,4.7 
-                c-3.2,0.4-8.3,1.1-14.2,0.9c-1.5-0.1-6.3-0.4-12-1.6c-5.7-1.2-11-3.1-15.8-3.7C6.5,9.2,0,10.8,0,10.8V0h283.5V9.7z">
-                </path>
-            </svg>
-        </div>
-
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6 text-center">
-                    <img class="img-circle img-thumbnail mb-15" 
-                        src="{{ asset('css/images/about/sq1.jpg') }}" 
-                        loading="lazy" 
-                        decoding="async"
-                        alt="yogintra">
-                    <div class="row">
-                        <div class="col-md-8 col-md-offset-2">
-                            <h2 class="mb-5">Power Vinyasa</h2>
-                            <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero omnis unde nesciunt?</h5>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-sm-6 text-center">
-                    <div class="section-4-title">Dietetics and Nutrition</div>
-                    <h4>
-                        Aziza Firdaus has 7 years of Clinical Experience in Dietetics & Nutrition 
-                        in the hospital sector and private clinics, as well as online coaching. 
-                        Her objective is to serve society with utmost speed, quality, and integrity, 
-                        achieving her goals effectively and efficiently. She has given good results to 
-                        her clients and played a role in their journey to becoming healthy.
-                    </h4>
-                </div>
-            </div>
-        </div>
-    </section>
-
     <section class="review-section">
         <div class="container">
             <div class="row">
