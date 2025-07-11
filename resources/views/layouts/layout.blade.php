5daa05f57f55151bd488ba2c3dc0ad0fe43f0db3
Ankangh001 <ankanghosh010@gmail.com>
2025-07-12 02:10:31 +0530

    up

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -187,7 +187,7 @@
 
     <script async src='https://d2mpatx37cqexb.cloudfront.net/delightchat-whatsapp-widget/embeds/embed.min.js'></script>
     <script>
-        var wa_btnSetting = {"btnColor":"#16BE45","ctaText":"","cornerRadius":40,"marginBottom":20,"marginLeft":20,"marginRight":20,"btnPosition":"right","whatsAppNumber":"919867291573","welcomeMessage":"Hello","zIndex":999999,"btnColorScheme":"light"};
+        var wa_btnSetting = {"btnColor":"#16BE45","ctaText":"","cornerRadius":40,"marginBottom":20,"marginLeft":20,"marginRight":20,"btnPosition":"left","whatsAppNumber":"919867291573","welcomeMessage":"Hello","zIndex":999999,"btnColorScheme":"light"};
         window.onload = () => {
             _waEmbed(wa_btnSetting);
         };
@@ -274,7 +274,7 @@
         #messageIcon {
             position: fixed;
             bottom: 20px;
-            right: 5%;
+            right: 20px;
             background-color: #ffd700;
             color: white;
             border-radius: 50%;
