3341bd4e8e907bb0aa0b31dd37710232317f7991
Ankangh001 <ankanghosh010@gmail.com>
2025-07-29 20:55:35 +0530

    fixes added

--- resources/views/front/all_retreat.blade.php
+++ resources/views/front/all_retreat.blade.php
@@ -97,7 +97,7 @@
       </div>
       <div class="col-md-7">
         <div class="mb-40">
-          <h1 class="text-uppercase text-center font-38 mt-0"><span class="text-theme-colored">MORE retreat</span> COMING SOON</h1>
+          <h2 class="text-uppercase text-center font-38 mt-0"><span class="text-theme-colored">MORE retreat</span> COMING SOON</h2>
         </div>
         @include('components.multi-step-form', ['app_setting' => $app_setting])
       </div>
