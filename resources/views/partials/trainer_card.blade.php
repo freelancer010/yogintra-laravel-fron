66238189ed6fe2626c9723ac942e86e33de6a444
Ankangh001 <ankanghosh010@gmail.com>
2025-08-01 02:24:04 +0530

    fixes

--- resources/views/partials/trainer_card.blade.php
+++ resources/views/partials/trainer_card.blade.php
@@ -1,11 +1,13 @@
 <div class="col-xs-12 col-sm-6 col-md-3 sm-text-center mb-30 mb-sm-30">
-  <div class="team-members text-center maxwidth400" onclick="open_modal({{ $i }})">
+  <div class="team-members text-center maxwidth400">
     <div class="team-thumb">
       <img class="img-fullwidth" id="imageresource_{{ $i }}" style="height: 200px; width: auto" alt="YogIntra" src="{{ $api }}/{{ $trainer['profile_image'] }}">
     </div>
     <div class="team-details">
       <div class="p-10" style="background-color: #01AEB7;">
-        <h4 class="team-title text-uppercase mt-0 mb-0"><a href="#">{{ $trainer['name'] }}</a></h4>
+        <h4 class="team-title text-uppercase mt-0 mb-0">
+          <a href="{{ route('trainer.show', $trainer['id']) }}"> {{ $trainer['name'] }} </a>
+        </h4>
         <p class="team-subtitle mt-0 mb-0">Age - {{ $age }}</p>
       </div>
       <div class="p-20 bg-white d-none">
