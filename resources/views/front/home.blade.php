96decafc4c6b76637792693ea84fad1b217ab405
Ankangh001 <ankanghosh010@gmail.com>
2025-08-22 03:27:34 +0530

    reoved inline css

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -8,6 +8,37 @@
         .yg-txt-right{
             text-align:right;
         }
+        .display-table-absolute {
+            position: absolute;
+            top: 0;
+        }
+        .heading-bold {
+            font-weight: 900;
+        }
+        .btn-theme-custom {
+            background: #e07f00 !important;
+        }
+        .mobile-home-banner {
+            width: 100%;
+            height: auto;
+            display: block;
+        }
+        .section-content-bg {
+            background-repeat: no-repeat;
+            background-size: auto;
+            min-height: 600px;
+        }
+        .section-pattern-bg {
+            background-repeat: repeat;
+            background-size: auto;
+        }
+        .section-parallax-bg {
+            background-position: center center;
+            background-size: cover;
+        }
+        .service-heading {
+            font-size: 16px;
+        }
         @media screen and (min-width: 1000px) and (max-width: 1200px) {
         .cst-font{
             font-size:28px !important;
@@ -225,18 +256,18 @@
                 <div class="carousel-item bg-img-cover">
                     <img src="{{ asset($slider->slider_image) }}" width="1519" height="854" loading="lazy" alt="YogIntra - {{ $slider->slider_heading }}" >
                     <div class="overlay"></div>
-                    <div class="display-table" style="position: absolute; top: 0;">
+                    <div class="display-table display-table-absolute">
                         <div class="display-table-cell">
                             <div class="container position-ab">
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="bg-white-transparent pt-20 pb-50 outline-border">
-                                            <h3 class="text-black-555 font-54" style="font-weight: 900">
+                                            <h3 class="text-black-555 font-54 heading-bold">
                                                 {{ $slider->slider_heading }}
                                             </h3>
                                             
                                             @if ($slider->slider_btn_name && $slider->slider_btn_link)
-                                                <a style="background: #e07f00 !important" class="btn btn-theme-colored btn-flat mt-15 high-contrast-btn"
+                                                <a class="btn btn-theme-colored btn-flat mt-15 high-contrast-btn btn-theme-custom"
                                                 href="{{ $slider->slider_btn_link }}">
                                                     {{ $slider->slider_btn_name }}
                                                 </a>
@@ -259,7 +290,7 @@
                 height="650"
                 decoding="async"
                 fetchpriority="high"
-                style="width: 100%; height: auto; display: block;"
+                class="mobile-home-banner"
             >
 
             <div class="position-absolute top-0 left-0 w-100 align-items-center p-15 pt-0">
@@ -278,7 +309,7 @@
 
     </section>
 
-    <section class="section-content-image" style="background-image: url('{{ asset($section_1->of_image) }}'); background-repeat: no-repeat; background-size: auto; min-height: 600px;">
+    <section class="section-content-image section-content-bg" style="background-image: url('{{ asset($section_1->of_image) }}');">
         <div class="container">
             <div class="section-title text-center">
                 <div class="row">
@@ -318,7 +349,7 @@
         </div>
     </section>
 
-    <section class="divider" style="background-image: url('{{ asset('assets/pattern-chakras-alt-color.webp') }}'); background-repeat: repeat; background-size: auto;">
+    <section class="divider section-pattern-bg" style="background-image: url('{{ asset('assets/pattern-chakras-alt-color.webp') }}');">
         <div class="elementor-shape elementor-shape-top" data-negative="false">
             <svg xmlns="https://www.w3.org/2000/svg" viewBox="0 0 283.5 27.8" preserveAspectRatio="xMidYMax slice">
                 <path class="elementor-shape-fill" d="M0 0v6.7c1.9-.8 4.7-1.4 8.5-1 9.5 1.1 11.1 6 11.1 6s2.1-.7 4.3-.2c2.1.5 2.8 2.6 2.8 2.6s.2-.5 1.4-.7c1.2-.2 1.7.2 1.7.2s0-2.1 1.9-2.8c1.9-.7 3.6.7 3.6.7s.7-2.9 3.1-4.1 4.7 0 4.7 0 1.2-.5 2.4 0 1.7 1.4 1.7 1.4h1.4c.7 0 1.2.7 1.2.7s.8-1.8 4-2.2c3.5-.4 5.3 2.4 6.2 4.4.4-.4 1-.7 1.8-.9 2.8-.7 4 .7 4 .7s1.7-5 11.1-6c9.5-1.1 12.3 3.9 12.3 3.9s1.2-4.8 5.7-5.7c4.5-.9 6.8 1.8 6.8 1.8s.6-.6 1.5-.9c.9-.2 1.9-.2 1.9-.2s5.2-6.4 12.6-3.3c7.3 3.1 4.7 9 4.7 9s1.9-.9 4 0 2.8 2.4 2.8 2.4 1.9-1.2 4.5-1.2 4.3 1.2 4.3 1.2.2-1 1.4-1.7 2.1-.7 2.1-.7-.5-3.1 2.1-5.5 5.7-1.4 5.7-1.4 1.5-2.3 4.2-1.1c2.7 1.2 1.7 5.2 1.7 5.2s.3-.1 1.3.5c.5.4.8.8.9 1.1.5-1.4 2.4-5.8 8.4-4 7.1 2.1 3.5 8.9 3.5 8.9s.8-.4 2 0 1.1 1.1 1.1 1.1 1.1-1.1 2.3-1.1 2.1.5 2.1.5 1.9-3.6 6.2-1.2 1.9 6.4 1.9 6.4 2.6-2.4 7.4 0c3.4 1.7 3.9 4.9 3.9 4.9s3.3-6.9 10.4-7.9 11.5 2.6 11.5 2.6.8 0 1.2.2c.4.2.9.9.9.9s4.4-3.1 8.3.2c1.9 1.7 1.5 5 1.5 5s.3-1.1 1.6-1.4c1.3-.3 2.3.2 2.3.2s-.1-1.2.5-1.9 1.9-.9 1.9-.9-4.7-9.3 4.4-13.4c5.6-2.5 9.2.9 9.2.9s5-6.2 15.9-6.2 16.1 8.1 16.1 8.1.7-.2 1.6-.4V0H0z"></path>
@@ -342,7 +373,7 @@
                                     <div class="">
                                         <img src="{{ asset($content_sec_2->os_image) }}" width="90" height="95" loading="lazy" decoding="async" alt="YogIntra Service Icon - {{ $content_sec_2->os_heading }}">
                                     </div>
-                                    <h3 style="font-size: 16px">{{ $content_sec_2->os_heading }}</h3>
+                                    <h3 class="service-heading">{{ $content_sec_2->os_heading }}</h3>
                                 </div>
                             @endforeach
                         </div>
@@ -352,7 +383,7 @@
         </div>
     </section>
 
-    <section class="divider types-of-yoga-section py-70" style="background-image: url('{{ asset('assets/parallax-decor2.png') }}'); background-position: center center; background-size: cover;">
+    <section class="divider types-of-yoga-section py-70 section-parallax-bg" style="background-image: url('{{ asset('assets/parallax-decor2.png') }}');">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-12">
