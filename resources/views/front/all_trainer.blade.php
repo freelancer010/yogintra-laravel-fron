ae57c57dcaa4a97591a3d6dd013dd52ffde4a3b9
Ankangh001 <ankanghosh010@gmail.com>
2025-10-26 14:06:39 +0530

    fixes made

--- resources/views/front/all_trainer.blade.php
+++ resources/views/front/all_trainer.blade.php
@@ -51,7 +51,6 @@
               <h4 class="team-title text-uppercase mt-0 mb-0">
                 <span style="color: white;">{{ $trainer['name'] }}</span>
               </h4>
-              <p class="team-subtitle mt-0 mb-0">Age - {{ $age }}</p>
               <p class="team-subtitle mt-0 mb-0"><i class="fa fa-map-marker"></i> {{ $trainer['city'] }}, {{ $trainer['state'] }}</p>
             </div>
             <div class="p-20 bg-white d-none">
