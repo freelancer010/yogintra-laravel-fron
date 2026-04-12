c469760b4fed1dbd6dd8a5d19cffd38b126afe59
Ankangh001 <ankanghosh010@gmail.com>
2026-04-12 20:40:06 +0530

    fixes added

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -518,7 +518,7 @@
                                     <div class="col-lg-3 col-md-3 col-sm-4 mb-30 wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.3s">
                                         <div class="yoga-service-item text-center">
                                             <img class="img-circle img-thumbnail mb-20" src="{{ asset($r_service->service_cat_image) }}" loading="lazy" decoding="async" alt="YogIntra Service Category - {{ $r_service->service_cat_name }}">
-                                            <h3 class="mb-15 fs-16">{{ $r_service->service_cat_name }}</h3>
+                                            <h2 class="mb-15 fs-16">{{ $r_service->service_cat_name }}</h2>
                                             <a href="{{ url('service/' . $r_service->service_cat_slug) }}" class="btn-sm-cs btn btn-success btn-primary-dark">Book Now</a>
                                         </div>
                                     </div>
