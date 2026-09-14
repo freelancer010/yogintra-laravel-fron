9265737874b3fc17c74e2e5ac9d001e3476fcf60
Ankangh001 <ankanghosh010@gmail.com>
2026-09-14 13:16:50 +0530

    Update blog page title to "Blogs for Yoga & Wellness | YogIntra"

--- resources/views/front/blog.blade.php
+++ resources/views/front/blog.blade.php
@@ -2,7 +2,7 @@
 
 @extends('layouts.layout')
 
-@section('meta_title', 'Yoga & Wellness Blog | Expert Tips, Poses & Mindfulness | YogIntra')
+@section('meta_title', 'Blogs for Yoga & Wellness | YogIntra')
 @section('meta_description', 'This blog is about my yoga journey. It started decades ago and has no end.')
 @section('meta_keywords', 'Online Yoga Classes India, Yoga Class in India, Best Yoga Institute In India, Best Yoga Center in India, Personal Yoga Trainer at Home, Best Yoga Classes in Mumbai.')
 
